7f1aa17ec83a8d0e544c29c7be2b4bbd6375b31d
Niran_Chan <niranshivu@gmail.com>
2026-07-11 22:10:03 +0800

    code is now asynchronous to ensure that stop is called immediately

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ import asyncio
 
 load_dotenv() 
 API_KEY=os.getenv("API_KEY")
-IS_START = False
+status_task = None
 
 whitelist = os.getenv("WHITELIST_TELEID")
 if(whitelist):
     whitelist = whitelist.strip().split(",")
+
 def get_status() -> dict:
     results = {
         "CPU,%" : None,
@@ -24,55 +25,67 @@ def get_status() -> dict:
             "Percent": None
         }
     }
-    cpu_percent = psutil.cpu_percent(interval=None,percpu=True)
-    results["CPU,%"] = cpu_percent
+    while True:
+        cpu_percent = psutil.cpu_percent(interval=None,percpu=True)
+        results["CPU,%"] = cpu_percent
 
-    #Memory
-    mem = psutil.virtual_memory()
-    results["Memory"]["Total"] = mem.total
-    results["Memory"]["Available"] =  mem.available
-    results["Memory"]["Used"] =  mem.used
-    results["Memory"]["Percent"] = mem.percent
-    
-    #Temperature
+        #Memory
+        mem = psutil.virtual_memory()
+        results["Memory"]["Total"] = mem.total
+        results["Memory"]["Available"] =  mem.available
+        results["Memory"]["Used"] =  mem.used
+        results["Memory"]["Percent"] = mem.percent
+        
+        #Temperature
 
-    try:
-        temps = psutil.sensors_temperatures()
-        results["Temperature,°C"] = temps
-    
-    except Exception:
-        pass
+        try:
+            temps = psutil.sensors_temperatures()
+            results["Temperature,°C"] = temps
+        
+        except Exception:
+            pass
+        
+        return results
 
-    return results
+async def send_status(chat_id, bot):
+    while True:
+        status = get_status()
+        formatted = "\n".join(f"{k:<20} {v}"for k, v in status.items())
+
+        await bot.send_message(
+            chat_id,
+            f"```\n{formatted}\n```",
+            parse_mode="MarkdownV2",
+        )
+
+        await asyncio.sleep(3600)  
 
 async def start(update: Update,context: ContextTypes.DEFAULT_TYPE)->None:
+    
     user_id = str(update.effective_user.id)
     if user_id not in whitelist:
         return 
-    await update.message.reply_text(f"Hi! I am PCAnxietyBot. I am here to help you with your anxious needs by reassuring you about the status of your pc every hour right here. Lets get started!")
-    #user_id = update.effective_user.id
-    #await update.message.reply_text(f"Your Telegram UserID is : {update.effective_user.id}")
-
-    global IS_START
-    IS_START = True
-    while(IS_START):
-        status = get_status()
-        formatted_str = "\n".join(f"{key:<20} {val}" for key, val in status.items())
-        await update.message.reply_text(
-        f"```\n{formatted_str}\n```",
-        parse_mode="MarkdownV2")
-        await asyncio.sleep(3)
-        break
-    return
+    
+    global status_task
+    if status_task is None or status_task.done():
+        await update.message.reply_text(f"Hi! I am PCAnxietyBot. I am here to help you with your anxious needs by reassuring you about the status of your pc every hour right here. Lets get started!")
+        #user_id = update.effective_user.id
+        #await update.message.reply_text(f"Your Telegram UserID is : {update.effective_user.id}")
+        status_task = asyncio.create_task(send_status(user_id,context.bot))
 
 async def stop(update: Update,context: ContextTypes.DEFAULT_TYPE):
-    global IS_START
-    IS_START = False
-    return 
+    global status_task
+    print("Attempting to stop")
+    if status_task:
+        status_task.cancel()
+        status_task = None
+
+    await update.message.reply_text("Monitoring stopped. Please /start again to start your hourly update")
 
 def run():
     app = ApplicationBuilder().token(API_KEY).build()
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("stop", stop))
     app.run_polling()
 
 if __name__ == '__main__':
