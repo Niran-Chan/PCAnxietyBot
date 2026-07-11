d19f53a8a36fc8a812548e0f97d3f05eb48abcf0
Niran_Chan <niranshivu@gmail.com>
2026-07-11 12:18:57 +0800

    Added whitelist capability feature. .env file must be defined.

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from dotenv import load_dotenv
 import os 
 import psutil
+from time import sleep
+import asyncio
 
 load_dotenv() 
 API_KEY=os.getenv("API_KEY")
-IS_START = True
+IS_START = False
 
-
+whitelist = os.getenv("WHITELIST_TELEID")
+if(whitelist):
+    whitelist = whitelist.strip().split(",")
 def get_status() -> dict:
     results = {
         "CPU,%" : None,
@@ -42,19 +46,34 @@ def get_status() -> dict:
     return results
 
 async def start(update: Update,context: ContextTypes.DEFAULT_TYPE)->None:
+    user_id = str(update.effective_user.id)
+    if user_id not in whitelist:
+        return 
     await update.message.reply_text(f"Hi! I am PCAnxietyBot. I am here to help you with your anxious needs by reassuring you about the status of your pc every hour right here. Lets get started!")
-    status = get_status()
-    formatted_str = "\n".join(f"{key:<20} {val}" for key, val in status.items())
-    await update.message.reply_text(
-    f"```\n{formatted_str}\n```",
-    parse_mode="MarkdownV2"
-)
+    #user_id = update.effective_user.id
+    #await update.message.reply_text(f"Your Telegram UserID is : {update.effective_user.id}")
 
+    global IS_START
+    IS_START = True
+    while(IS_START):
+        status = get_status()
+        formatted_str = "\n".join(f"{key:<20} {val}" for key, val in status.items())
+        await update.message.reply_text(
+        f"```\n{formatted_str}\n```",
+        parse_mode="MarkdownV2")
+        await asyncio.sleep(3)
+        break
+    return
 
+async def stop(update: Update,context: ContextTypes.DEFAULT_TYPE):
+    global IS_START
+    IS_START = False
+    return 
 
+def run():
+    app = ApplicationBuilder().token(API_KEY).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
 
-app = ApplicationBuilder().token(API_KEY).build()
-
-app.add_handler(CommandHandler("start", start))
-
-app.run_polling()
+if __name__ == '__main__':
+    run()
